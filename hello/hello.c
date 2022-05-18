212d1c2d3d8373216b23d3092a2523d242391eab
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-18 01:59:26 +0000

    Wed, May 18, 2022, 1:59 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -2,6 +2,6 @@
 #include <cs50.h>
 int main(void)
 {
-    string name = get_string("What is your name? ");
+    string name = get_string("What is your name?\n");
     printf("hello, %s\n", name);
 }
