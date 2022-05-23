62c0ea65003e83fd17ad80612dbdc07e8e02a35b
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-23 23:15:35 +0000

    Mon, May 23, 2022, 11:15 PM +00:00

--- program2/program2.c
+++ program2/program2.c
@@ -3,6 +3,6 @@
 int main() {
     int myNum=15;
     char myChar='m';
-    printf("myNum= %d, myChar=%c \n",myChar, myNum);
+    printf("myNum= %d, myChar=%c \n", myNum, myChar);
     return 0;
     }
