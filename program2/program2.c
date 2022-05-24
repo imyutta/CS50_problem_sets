fb0fb27bfbbcab17ea11523658f84653cba3e76e
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-24 21:56:04 +0000

    Tue, May 24, 2022, 9:56 PM +00:00

--- program2/program2.c
+++ program2/program2.c
@@ -3,13 +3,13 @@
 int main() {
     int time=20;
     switch (time) {
-        case >0:
+        case time>0:
           printf("positive");
           break;
-        case <0:
+        case time<0:
           printf("negative");
           break;
-        case ==0:
+        case time==0:
           printf("=0");
           break;
 
