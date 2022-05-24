d3bcdf91c51b385f543454986e937eafdba23b5c
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-24 23:14:43 +0000

    Tue, May 24, 2022, 11:14 PM +00:00

--- program2/program2.c
+++ program2/program2.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int i=1;
+    int i;
 
-    do {
+    for (i=1; i<10; i++) {
       printf("%d\n",i);
-      i++;
     }
-    while (i<5);
      return 0;
 }
 
