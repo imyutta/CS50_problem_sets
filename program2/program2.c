21924bee8aa5daefc250ea3c895abfc4bc1266d0
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-24 20:50:56 +0000

    Tue, May 24, 2022, 8:50 PM +00:00

--- program2/program2.c
+++ program2/program2.c
@@ -2,9 +2,14 @@
 
 int main() {
     int x=20;
-    int y=20;
-    if (x>=y) {
-        printf("x>=y\n");
+    int y=4;
+    if (x==y) {
+        printf("x=y\n");
+    }else if(x>y){
+    printf("x>y\n");
+    }
+    else {
+        printf("x<y\n");
     }
         return 0;
 }
