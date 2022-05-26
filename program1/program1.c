087b3ee46ddf0b5544bdcd575e0ce3b56abf6cfe
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-26 04:51:25 +0000

    Thu, May 26, 2022, 4:51 AM +00:00

--- program1/program1.c
+++ program1/program1.c
@@ -10,9 +10,9 @@ int main(void)
         n=get_int("Width: ");
     }
     while (n<1);
-    for (int i=0; i<4; i++)
+    for (int i=0; i<n; i++)
     {
         printf("?");
     }
-    printf("\n);
+    printf("\n");
 }
