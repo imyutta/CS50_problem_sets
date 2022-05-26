917183522c122aa71ac38a42925a08b13656c931
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-26 04:15:52 +0000

    Thu, May 26, 2022, 4:15 AM +00:00

--- program2/program2.c
+++ program2/program2.c
@@ -5,12 +5,12 @@ float discount(float price, int percentage);
 
 int main(void) {
    float regular = get_float("Regular Price: ");
-   int percent_off = get_int("Percent Off: ")
+   int percent_off = get_int("Percent Off: ");
    float sale = discount(regular, percent_off);
    printf("Sale Price: %.2f\n", sale);
 }
 
 float discount(float price, int percentage)
 {
-  return price*(100-percentage)/100);
+  return price*(100-percentage)/100;
 }
