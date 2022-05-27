19f1918d2a4d166c6792b4ba801ae17323934128
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-27 01:13:57 +0000

    Fri, May 27, 2022, 1:13 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,15 +5,18 @@ int main(void)
 {
     int h,i;
 
-    // Ask user for a positive number from 1 to 8
+// Ask user for a positive number from 1 to 8
     do
     {
       h=get_int("Height: \n");
     }
     while (h <1 || h > 8);
 
+//the amount of lines will be
     for (i = 0; i < h; i++)
     {
-      
+//the amount of #'s will be
+       p = p +
+
     }
 }
