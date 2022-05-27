a7516d0369e8db77ea570ac4fa771eb9952c2043
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-27 21:06:52 +0000

    Fri, May 27, 2022, 9:06 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,7 +4,11 @@
 int main(void)
 {
     long int card_number = get_long(What is your card number? );
+// sum1 of every other digit miltiplied by 2
 
 
+// sum2 where sum1 plus the sum of digits thah weren't multiplied by 2
+
+// sum2% = 0?
 
 }
