b5abef50ea86f546be15ae247115a418434fa050
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-27 00:31:18 +0000

    Fri, May 27, 2022, 12:31 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int h,i;
+
+    // Ask user for a positive number from 1 to 8
+    do
+    {
+      h=get_int("Height: \n");
+    }
+    while (h <1 || h > 8);
+
+    for
 }
