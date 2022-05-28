60c4cc7788e8457436a88a0b8468f4234a724184
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-28 23:05:54 +0000

    Sat, May 28, 2022, 11:05 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,52 +7,48 @@ int int_length(long int);
 
 int main(void)
 {
-    long int card_number = get_long("What is your card number? ");
+  long int card_number = get_long("What is your card number? ");
 
-    long int n;
-    int mult_digit2;
-    int sum_mult_digit2 = 0;
-    int sum_digit1 = 0;
+  long int n;
+  int mult_digit2;
+  int sum_mult_digit2 = 0;
+  int sum_digit1 = 0;
 
-    for (n=10; n <= 1000000000000000; n*=100)
-    {
+  for (n=10; n <= 1000000000000000; n*=100)
+  {
 
-      mult_digit2 = (floor((card_number % (n * 10)) / n)) * 2;    // multiply every 2nd digit (counting from the 2nd from the end) by 2
-      sum_mult_digit2 += (mult_digit2 % 10) + (floor(mult_digit2 / 10));    // add those mult_digits2 together (not the numbers, but their digits)
+    mult_digit2 = (floor((card_number % (n * 10)) / n)) * 2;    // multiply every 2nd digit (counting from the 2nd from the end) by 2
+    sum_mult_digit2 += (mult_digit2 % 10) + (floor(mult_digit2 / 10));    // add those mult_digits2 together (not the numbers, but their digits)
 
-    }
-
-
-     for (n=1; n<= 100000000000000; n *= 100)
-     {
-        sum_digit1 = sum_digit1 + (floor (card_number % (n*10)) / n);     // sum of every 2nd digit (counting from the 1st!!! from the end)
-     }
+  }
 
 
-     //checking whether it is AMEX or MASTERCARD or VISA
-     if ((sum_mult_digit2 + sum_digit1) % 10 == 0 && int_length(card_number)==15 && (floor (card_number / 10000000000000) == 34 || floor (card_number / 10000000000000) == 37))
-     {
-       printf("AMEX\n");
+  for (n=1; n<= 100000000000000; n *= 100)
+  {
+    sum_digit1 = sum_digit1 + (floor (card_number % (n*10)) / n);     // sum of every 2nd digit (counting from the 1st!!! from the end)
+  }
+
+
+  
+
+  //checking whether it is AMEX or MASTERCARD or VISA
+  if ((sum_mult_digit2 + sum_digit1) % 10 == 0 && int_length(card_number)==15 && (floor (card_number / 10000000000000) == 34 || floor (card_number / 10000000000000) == 37))
+  {
+    printf("AMEX\n");
     //the first digit=34 or 37?
-     }
-
-
-     else if ((sum_mult_digit2 + sum_digit1) % 10 ==0 && int_length(card_number)==16 && (floor (card_number / 10000000000000) == 51 || floor (card_number / 10000000000000) == 52 || floor (card_number / 10000000000000) == 53 || floor (card_number / 10000000000000) == 54 || floor (card_number / 10000000000000) == 55))
-     {
-      printf("MASTERCARD\n");
-     }
-
-
-
-     else if ((sum_mult_digit2 + sum_digit1)==0 && (int_length(card_number)==13 || int_length(card_number)==16) && floor (card_number / 10000000000000) == 4)
-     {
-       printf("VISA\n");
-     }
-
-     else
-     {
-         printf("INVALID\n");
-     }
+  }
+  else if ((sum_mult_digit2 + sum_digit1) % 10 ==0 && int_length(card_number)==16 && (floor (card_number / 10000000000000) == 51 || floor (card_number / 10000000000000) == 52 || floor (card_number / 10000000000000) == 53 || floor (card_number / 10000000000000) == 54 || floor (card_number / 10000000000000) == 55))
+  {
+    printf("MASTERCARD\n");
+  }
+  else if ((sum_mult_digit2 + sum_digit1)==0 && (int_length(card_number)==13 || int_length(card_number)==16) && floor (card_number / 10000000000000) == 4)
+  {
+    printf("VISA\n");
+  }
+  else
+  {
+    printf("INVALID\n");
+  }
 
 
     // ne rabotaet while (n <= pow(n,15));
