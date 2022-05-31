e964eddd8861f74c704383a3a398288c2755ed23
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-31 18:58:06 +0000

    Tue, May 31, 2022, 6:58 PM +00:00

--- program2/program21.c
+++ program2/program21.c
@@ -2,11 +2,14 @@
 
 int main ()
 {
-    int myNumbers[] = {25, 50, 75, 100};
+    int myNumbers[4];
+    myNumbers[0] = 25;
     int i;
     for (i = 0; i < 4; i++)
     {
         printf("%d\n", myNumbers[i]);
     }
+
+
 
 }
