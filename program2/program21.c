2074cb6ccc5715a3a48fa0a7baaebdd91c09472a
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-31 21:32:32 +0000

    Tue, May 31, 2022, 9:32 PM +00:00

--- program2/program21.c
+++ program2/program21.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  where are you?
-  if us printf (today is 6 2)
-  else printf (today 2  6)
-  printf you are 31;
-  printf you 
+  int yourAge = 30;
+  if 
 
 
 }
