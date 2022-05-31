9f3de5d0d9afd0ade489b1286804eff0bb0a390f
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-31 22:26:55 +0000

    Tue, May 31, 2022, 10:26 PM +00:00

--- program2/program21.c
+++ program2/program21.c
@@ -2,17 +2,7 @@
 #include <cs50.h>
 
 int main() {
-  int yourAge = 30;
-  char date[] = "Today is June 1st!\n";
-  char day = get_char("Is it a new day today? y/n\n");
-  if (day == 'y' || day == 'Y') {
-    date[14] = '2';
-    date[15] = 'n';
-    date[16] = 'd';
-    printf("%s", date);
-    printf("You are %i!\n", yourAge += 1);
-    printf("Happy birthday!\n");
-    printf("I love you!\n");
+ 
 
   }
 
