a9e15cbff4ff8caa77220579b48622212e3eb74f
Yutta <105465153+imyutta@users.noreply.github.com>
2022-05-31 21:45:46 +0000

    Tue, May 31, 2022, 9:45 PM +00:00

--- program2/program21.c
+++ program2/program21.c
@@ -4,6 +4,7 @@ int main() {
   int yourAge = 30;
   char date[] = "Today is June 01!\n";
   int day = get_int("Is it June 2nd today? yes/no\n");
+  if (day == yes || day == YES || day == Yes || day == y || day == Y) {}
   date[15] = '2';
   printf("%s", date);
 
