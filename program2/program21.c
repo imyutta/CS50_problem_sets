5891ecdb7a63a17139dd429e756dcf447e4c7479
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-01 04:57:18 +0000

    Wed, Jun 1, 2022, 4:57 AM +00:00

--- program2/program21.c
+++ program2/program21.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
 
+struct myStructure
+{
+  int myNum;
+  char myLetter;
+};
+
 int main() {
- 
+  struct myStructure s1;
+  s1.myNum = 13;
+  s1.myLetter = 'B';
+
+  printf("my number: %d\n", s1.myNum);
+  printf("my letter: %c\n", s1.myLetter);
 
   }
 
@@ -10,4 +21,3 @@ int main() {
 
 
 
-}
