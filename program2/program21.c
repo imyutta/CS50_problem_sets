a56ff83cf5eca30190799f681b6b2f49a6ddf865
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-01 05:13:27 +0000

    Wed, Jun 1, 2022, 5:13 AM +00:00

--- program2/program21.c
+++ program2/program21.c
@@ -12,6 +12,7 @@ int main() {
   struct myStructure s1 = {13, 'B', "Some text"};
   struct myStructure s2;
   s2=s1;
+  s2.myNum = 30;
 
   printf("%d, %c, %s\n", s2.myNum, s2.myLetter, s2.myString);
 
