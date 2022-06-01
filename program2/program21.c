e0a3ef6d60e3cfb6a8e0767811e3b22a87f43072
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-01 05:31:19 +0000

    Wed, Jun 1, 2022, 5:31 AM +00:00

--- program2/program21.c
+++ program2/program21.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-
-struct myStructure
-{
-  int myNum;
-  char myLetter;
-  char myString[30];
-};
 
 int main() {
-  struct myStructure s1 = {13, 'B', "Some text"};
-  struct myStructure s2;
-  s2=s1;
-  s2.myNum = 30;
+  // Create a string
+  char firstName[30];
 
-  printf("%d, %c, %s\n", s2.myNum, s2.myLetter, s2.myString);
+  // Ask the user to input some text
+  printf("Enter your first name: \n");
 
+  // Get and save the text
+  scanf("%s", firstName);
 
-  }
+  // Output the text
+  printf("Hello %s.", firstName);
 
+  return 0;
 
-
+}
 
 
