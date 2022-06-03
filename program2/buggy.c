45cbd3e225aadd745dba3fe7dc1a749bb8bfa0ff
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-03 20:29:18 +0000

    Fri, Jun 3, 2022, 8:29 PM +00:00

--- program2/buggy.c
+++ program2/buggy.c
@@ -5,12 +5,10 @@ int get_negative_int(void);
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
+    string s = "Hi!";
 
 
-    printf("%i %i %i\n", (int) c1, (int) c2, (int) c3);
+    printf("%s\n", s);
 }
 
 
