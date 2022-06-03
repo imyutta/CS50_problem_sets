f056dead65002ce02f73b051196415bb2c0c8599
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-03 22:54:01 +0000

    Fri, Jun 3, 2022, 10:54 PM +00:00

--- program2/argc.c
+++ program2/argc.c
@@ -3,10 +3,13 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 2)
+    if (argc != 2)
     {
+        printf("missing command-line argument\n");
+        return 1;
+    }
         printf("hello, %s\n", argv[1]);
     }
-    
+
 }
 
