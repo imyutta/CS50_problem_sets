d51aa637504f94212cfead6f6a20a2fa2886b25d
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-03 22:36:44 +0000

    Fri, Jun 3, 2022, 10:36 PM +00:00

--- program2/buggy.c
+++ program2/buggy.c
@@ -1,12 +1,19 @@
+// #include <cs50.h>
+// #include <stdio.h>
+
+// int main(int argc, string argv[])
+// {
+//     if (argc == 2)
+//     {
+//         printf("hello, %s\n", argv[1]);
+//     }
+//     printf("argc = %i\n", argc);
+// }
+
 #include <cs50.h>
 #include <stdio.h>
 
 int main(int argc, string argv[])
 {
-    if (argc == 2)
-    {
-        printf("hello, %s\n", argv[1]);
-    }
-    printf("argc = %i\n", argc);
+    printf("hello, %s\n", argv[0]);
 }
-
