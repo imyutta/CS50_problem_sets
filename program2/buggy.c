7d38ee318b0072b6f4f3781b31113ac3e3d04173
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-03 22:24:50 +0000

    Fri, Jun 3, 2022, 10:24 PM +00:00

--- program2/buggy.c
+++ program2/buggy.c
@@ -3,24 +3,10 @@
 #include <string.h>
 
 
-int main(void)
+int main(int argc, string argv [])
 {
-    string s = get_string("Before: ");
-    printf("After: ");
-    for (int i = 0, n=strlen(s); i < n; i++)
-    {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            printf("%c", s[i] - 32);
-        }
-        else
-        {
-            printf("%c", s[i]);
-        }
+    printf("hello, %s\n", argv[0]);
 
-        
-    }
-    printf("\n");
 }
 
 
