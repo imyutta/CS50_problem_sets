06eebea5e49cc2a43742ecaded2c12252b0038da
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-06 20:00:53 +0000

    Mon, Jun 6, 2022, 8:00 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    //promt the user for a key
 
+    //check if argc = 2
+
+    //check if the key contains only letters
+
+    //check if there 26 letters
 }
