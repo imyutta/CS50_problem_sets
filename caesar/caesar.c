7d0dad17a4c71d32a17d4aa68418210602730234
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-06 01:41:25 +0000

    Mon, Jun 6, 2022, 1:41 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,7 +3,7 @@
 
 int main(int argc, string argv[])
 {
-    //get a non-negative integer k
+    //check if there is only one command-line argument
     if (argc != 2)
     {
         printf ("Usage: ./caesar key\n");
@@ -13,6 +13,16 @@ int main(int argc, string argv[])
     {
         return 0;
     }
+
+    //check whether argv[1] is a digit
+
+    //make int argv[1]
+
+    //ask user for text
+
+    // for each caracter in the user's text:
+
+    //rotate each letter
 
 
 
