a3e59cc123b23f4bbbf8b244a3c8674026649b6d
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-07 05:07:40 +0000

    Tue, Jun 7, 2022, 5:07 AM +00:00

--- program/program.c
+++ program/program.c
@@ -5,12 +5,13 @@
 int main(void)
 {
   // Declare an array of four integers:
-  int myNumbers[3];
+  int myNumbers[4];
 
   // Add elements to it
   myNumbers[0] = 25;
   myNumbers[1] = 50;
   myNumbers[2] = 75;
+  myNumbers[3] = 75;
 
 
   printf("size of int %lu\n", sizeof(myNumbers));
