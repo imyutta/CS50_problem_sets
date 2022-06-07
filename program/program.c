f4d655f44db49462f60e0650a01a213d1b65518e
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-07 05:04:03 +0000

    Tue, Jun 7, 2022, 5:04 AM +00:00

--- program/program.c
+++ program/program.c
@@ -1,27 +1,23 @@
-// #include <cs50.h>
-// #include <stdio.h>
-// #include <string.h>
-
-// int main(void)
-// {
-//     string s = "bgn";
-
-//     printf("%c\n", s[0]);
-
-//     s[0] = 'B';
-//     printf("%c\n", s[0]);
-
-//     //printf("%c\n", s[0]);
-
-// }
-
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
+  // Declare an array of four integers:
+  int myNumbers[3];
+
+  // Add elements to it
+  myNumbers[0] = 25;
+  myNumbers[1] = 50;
+  myNumbers[2] = 75;
+
+
+  printf("size of int %lu\n", sizeof(myNumbers));
+
+
     char s[] = "HI!";
     printf("%s \n", s);
 
-    printf("%lu\n", sizeof(s));
+    printf("size of char %lu\n", sizeof(s));
 }
