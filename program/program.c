4c85989aeb2ee0b37bc6910990aba6bf18d381e1
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-07 02:30:29 +0000

    Tue, Jun 7, 2022, 2:30 AM +00:00

---
+++ program/program.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int array[3];
+    printf("%i", array[0]);
+    printf("%i", array[1]);
+    printf("%i", array[2]);
+    printf("%i", array[3]);
+    printf("%i", array[4]);
+}
