bede2571a78543e3bf7ebd326452ac7cc08268f3
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-07 02:37:26 +0000

    Tue, Jun 7, 2022, 2:37 AM +00:00

--- program/program.c
+++ program/program.c
@@ -1,12 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string array[3];
-    printf("%s", array[0]);
-    printf("%s", array[1]);
-    printf("%s", array[2]);
+    array[0] = "0";
+    array[1] = "1";
+    array[2] = "2";
+    printf("%s\n", array[0]);
+    printf("%s\n", array[1]);
+    printf("%s\n", array[2]);
+    printf("%i\n", strlen(array));
 
 
 }
