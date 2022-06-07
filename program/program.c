40dbc8235050a09458c0644b30432c2707299034
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-07 02:58:07 +0000

    Tue, Jun 7, 2022, 2:58 AM +00:00

--- program/program.c
+++ program/program.c
@@ -1,18 +1,24 @@
+// #include <cs50.h>
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(void)
+// {
+//     string s = "bgn";
+//     s[0] = 'v';
+//     s[1] = 'f';
+//     s[2] = 'b';
+//     printf("%c\n", s[0]);
+//     printf("%c\n", s[1]);
+//     printf("%c\n", s[2]);
+
+// }
+
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-    string s = "bgn";
-    s[0] = 'v';
-    s[1] = 'f';
-    s[2] = 'b';
-    printf("%c\n", s[0]);
-    printf("%c\n", s[1]);
-    printf("%c\n", s[2]);
-
-
-
-
+    string s = "HI!";
+    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
 }
