30f874b3e6c51c16e201981c08673d884f95bf55
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-08 04:24:02 +0000

    Wed, Jun 8, 2022, 4:24 AM +00:00

--- program/program.c
+++ program/program.c
@@ -17,6 +17,18 @@ int main(void)
     people[0].name = "Carter";
     people[0].number = "+1-949-468-1000";
 
-    people[1].name = "Carter";
-    people[1].number = "+1-949-468-1000";
+    people[1].name = "David";
+    people[1].number = "+1-849-468-1000";
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(people[i].name, "David") == 0)
+        {
+            printf("Found %s\n", people[i].number);
+            return 0;
+        }
+    }
+
+printf("Not found\n");
+return 1;
 }
