27dc2502ab875336243dc043244396a4bbdc753f
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-08 05:58:04 +0000

    Wed, Jun 8, 2022, 5:58 AM +00:00

--- program/program.c
+++ program/program.c
@@ -1,13 +1,41 @@
+// #include <cs50.h>
+// #include <stdio.h>
+// #include <string.h>
+
+
+// void draw(int n);
+
+// int main(void)
+// {
+//     int height = get_int("Height: ");
+//     draw(height);
+// }
+
+// void draw(int n)
+// {
+//     if (n <= 0)
+//     {
+//         return;
+//     }
+
+//     draw(n-1);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("#");
+//     }
+//     printf("\n");
+// }
+
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
-
 
 void draw(int n);
 
 int main(void)
 {
     int height = get_int("Height: ");
+
     draw(height);
 }
 
@@ -18,7 +46,7 @@ void draw(int n)
         return;
     }
 
-    draw(n-1);
+    draw(n - 1);
 
     for (int i = 0; i < n; i++)
     {
