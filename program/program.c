e0d04e69062107c191b209c58982c1f646f74889
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-08 04:19:32 +0000

    Wed, Jun 8, 2022, 4:19 AM +00:00

--- program/program.c
+++ program/program.c
@@ -12,5 +12,11 @@ person;
 
 int main(void)
 {
+    person people[2];
 
+    people[0].name = "Carter";
+    people[0].number = "+1-949-468-1000";
+
+    people[1].name = "Carter";
+    people[1].number = "+1-949-468-1000";
 }
