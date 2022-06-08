91ba7728331bd68758abbcf67c839fcd6f8211e4
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-08 01:54:56 +0000

    Wed, Jun 8, 2022, 1:54 AM +00:00

--- program/program.c
+++ program/program.c
@@ -5,8 +5,15 @@
 int main(void)
 {
     string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
-    names[0] = "BILL";
 
-            printf("%s\n", names[0]);
-
+    for (int i = 0; i < 7; i++)
+    {
+        if (names[i] == "Ron")
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
