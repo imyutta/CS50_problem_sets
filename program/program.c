b03332ca9f18f684c552a21f81d424ed33da931e
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-08 05:45:35 +0000

    Wed, Jun 8, 2022, 5:45 AM +00:00

--- program/program.c
+++ program/program.c
@@ -3,32 +3,8 @@
 #include <string.h>
 
 
-typedef struct
-{
-    string name;
-    string number;
-}
-person;
 
 int main(void)
 {
-    person people[2];
-
-    people[0].name = "Carter";
-    people[0].number = "+1-949-468-1000";
-
-    people[1].name = "David";
-    people[1].number = "+1-849-468-1000";
-
-    for (int i = 0; i < 2; i++)
-    {
-        if (strcmp(people[i].name, "David") == 0)
-        {
-            printf("Found %s\n", number);
-            return 0;
-        }
-    }
-
-printf("Not found\n");
-return 1;
+    
 }
