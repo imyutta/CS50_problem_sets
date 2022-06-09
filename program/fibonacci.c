6b32fcbdd0c5a32b0e13b263f55f64287f877df0
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-09 21:34:14 +0000

    Thu, Jun 9, 2022, 9:34 PM +00:00

---
+++ program/fibonacci.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+int fibonacci(int n);
+
+int main(void)
+{
+    int n = get_int("n= ");
+
+    int answer = fibonacci(n);
+    printf("answer = %i\n", answer);
+}
+
+int fibonacci(int n)
+{
+    if (n == 1)
+        return 0;
+    else if ((n % 2) == 0)
+        return 1 + collatz(n/2);
+    else
+        return 1 + collatz(3*n+1);
+}
