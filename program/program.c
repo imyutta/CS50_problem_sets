0b1d708915d143839dbc262b23eb69b3457d6d9c
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-09 20:59:43 +0000

    Thu, Jun 9, 2022, 8:59 PM +00:00

--- program/program.c
+++ program/program.c
@@ -1,33 +1,33 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int collatz(int n);
+int collatz(int n, int steps);
 
 int main(void)
 {
     int n = get_int("n= ");
     int steps = 0;
-    int allSteps = collatz(n);
+    int allSteps = collatz(n, steps);
     printf("Steps = %i\n", allSteps);
 }
 
-int collatz(int n)
+int collatz(int n, int steps)
 {
-    int steps;
+
     if (n == 1)
     {
-        return 0;
+        return steps;
     }
     else if (n % 2 == 0)
     {
 
         steps++;
-        return collatz(n / 2);
+        return collatz(n / 2, steps++);
     }
     else
     {
         steps++;
-        return collatz(3 * n + 1);
+        return collatz((3 * n + 1), steps++);
     }
 
 }
