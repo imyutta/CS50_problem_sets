5c5b9106bf7e4bed170fe2561499660d2d8c0aa9
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-09 21:39:24 +0000

    Thu, Jun 9, 2022, 9:39 PM +00:00

--- program/fibonacci.c
+++ program/fibonacci.c
@@ -14,13 +14,11 @@ int main(void)
 
 int fibonacci(int n)
 {
-    0
-    1
-    
+
     if (n == 1)
         return 0;
-    else if ((n % 2) == 0)
-        return 1 + collatz(n/2);
+    else if (n == 2)
+        return 1;
     else
-        return 1 + collatz(3*n+1);
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
