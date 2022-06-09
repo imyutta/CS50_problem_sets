324d06ae0136b48ae5b6466a5f4ca5779f729d9a
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-09 05:42:52 +0000

    Thu, Jun 9, 2022, 5:42 AM +00:00

--- program/program.c
+++ program/program.c
@@ -1,56 +1,16 @@
-// #include <cs50.h>
-// #include <stdio.h>
-// #include <string.h>
-
-
-// void draw(int n);
-
-// int main(void)
-// {
-//     int height = get_int("Height: ");
-//     draw(height);
-// }
-
-// void draw(int n)
-// {
-//     if (n <= 0)
-//     {
-//         return;
-//     }
-
-//     draw(n-1);
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         printf("#");
-//     }
-//     printf("\n");
-// }
-
 #include <cs50.h>
 #include <stdio.h>
 
-void draw(int n);
+int collatz(int n);
 
 int main(void)
 {
-    int height = get_int("Height: ");
 
-    draw(height);
 }
 
-void draw(int n)
+int collatz(int n)
 {
-    if (n <= 0)
-    {
-        return;
-    }
-
-    draw(n - 1);
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("#");
-    }
-    printf("\n");
+    for (int i = 1; i < )
+    if (n == 1)
+    return
 }
