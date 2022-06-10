bcb6b1b929c2ddfd5642c24c5f22810d088a954e
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-10 23:27:08 +0000

    Fri, Jun 10, 2022, 11:27 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,9 +131,10 @@ bool vote(int voter, int rank, string name)
     //update global preferences of the voter
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates.[i]name, name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             preferences[voter][rank] +=1;
+            return true;
         }
     }
     return false;
