86dbd3cf71732bc345f481d91399f3ee185b2151
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-10 23:11:14 +0000

    Fri, Jun 10, 2022, 11:11 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,7 +128,7 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     //check if name is valid
-    
+    if strcmp()
 
     //update global preferences of the voter
     return false;
