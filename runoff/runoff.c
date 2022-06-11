85d20796c8f15d01dd20127b809f8e80438c9ec1
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-11 01:04:47 +0000

    Sat, Jun 11, 2022, 1:04 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -201,6 +201,10 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false )
+    }
     return false;
 }
 
