d9289bcb56b6d1be5b35c6a849f755c575238025
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-11 00:48:05 +0000

    Sat, Jun 11, 2022, 12:48 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -186,10 +186,10 @@ int find_min(void)
     // for uneliminated candidates
     // find the one with minimum votes
     // return minimum vote total
-    int min;
-    for (int i = 0; i < candidate_count; i++)
+    int min = candidates[0].votes;
+    for (int i = 1; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == false)
+        if (candidates[i].eliminated == false && candidates[i])
         {
 
         }
