eafe6578be3f88c5dbf3508915002dcf8e475d7b
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-11 02:34:04 +0000

    Sat, Jun 11, 2022, 2:34 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -134,7 +134,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            preferences[voter][rank] += 1;
+            preferences[voter][rank] = i;
             return true;
         }
     }
