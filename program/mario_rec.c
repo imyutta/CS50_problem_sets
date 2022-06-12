f496c6a1ba2f52c693914de0184ada2318d5acd8
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-12 05:26:09 +0000

    Sun, Jun 12, 2022, 5:26 AM +00:00

---
+++ program/mario_rec.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+void mario(int n);
+
+int main(void)
+{
+    int n = get_int("height= ");
+    mario(n);
+}
+
+void mario(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+
+    }
+}
