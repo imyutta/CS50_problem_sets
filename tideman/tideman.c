ca16749df76931c0341435549646de274d604ccc
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-13 23:05:19 +0000

    Mon, Jun 13, 2022, 11:05 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,6 +100,8 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // look for a candidate called NAME
+    // if found, update ranks and return true.
+    // if not found, return false
     for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(candidates[i], name) == 0)
@@ -107,8 +109,7 @@ bool vote(int rank, string name, int ranks[])
             
         }
     }
-    // if found, update ranks and return true.
-    // if not found, return false
+
     return false;
 }
 
