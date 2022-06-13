1daf0f3e056aa57ce2b270283a6f7c2d521c3ba6
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-13 23:43:43 +0000

    Mon, Jun 13, 2022, 11:43 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -125,7 +125,7 @@ void record_preferences(int ranks[])
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if ()
+            if (ranks[])
             preferences[]
         }
     }
