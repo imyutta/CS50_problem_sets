96c9066d457697fe3ffc2bfa44c7383970e09aa4
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-13 23:34:05 +0000

    Mon, Jun 13, 2022, 11:34 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,7 +117,10 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    preferences[ranks[0]][j]
+    if (ranks[0] != j)
+    {
+        preferences[ranks[0]][j] += 1;
+    }
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
