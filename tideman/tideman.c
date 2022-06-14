364c6626c6ea03c539ec4f15b56339324cbf4013
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-14 19:23:14 +0000

    Tue, Jun 14, 2022, 7:23 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -160,7 +160,7 @@ void sort_pairs(void)
     int current_loser;
     int index_for_replacement;
     int max_score;
-    for (int i = 0; i < pair_count; i++)
+    for (int i = 0; i < pair_count - 1; i++)
     {
         for (int j = i + 1; j < pair_count; j++)
         {
