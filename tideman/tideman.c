a16db991af474fdc04d0acd7865d6261fc2f83ad
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-14 21:05:58 +0000

    Tue, Jun 14, 2022, 9:05 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -207,8 +207,10 @@ void lock_pairs(void)
 void print_winner(void)
 {
     // TODO
+    int current_winner;
     for (int j = 0; j < candidate_count; j++)
     {
+        current_winner = j;
         for ( int i = 0; i < candidate_count; i++)
         {
             if ( locked[i][j] == true)
