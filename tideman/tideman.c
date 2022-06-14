718bb90158917282c6572e570ac40b9a91eecd63
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-14 02:25:58 +0000

    Tue, Jun 14, 2022, 2:25 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -119,23 +119,10 @@ void record_preferences(int ranks[])
     for (int i = 0; i < candidates_count; i++)
     {
         for (int j = i + 1; j < candidates_count; j++)
-        preferences[ranks[i]][ranks[j]] += 1;
-
-        preferences[ranks[i]][ranks[i + 1]] += 1;
-        preferences[ranks[i]][ranks[i + 2]] += 1;
-        preferences[ranks[i]][ranks[i + 3]] += 1;
-        preferences[ranks[i]][ranks[i + 4]] += 1;
-
-        preferences[ranks[i + 1]][ranks[i + 2]] += 1;
-        preferences[ranks[i + 1]][ranks[i + 3]] += 1;
-        preferences[ranks[i + 1]][ranks[i + 4]] += 1;
-
-
+        {
+            preferences[ranks[i]][ranks[j]] += 1;
+        }
     }
-    }
-
-    }
-
     return;
 }
 
