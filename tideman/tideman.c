6ea425a9e1fb584a27804d99a3e5bb2b426e3301
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-15 03:59:21 +0000

    Wed, Jun 15, 2022, 3:59 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -219,12 +219,25 @@ void print_winner(void)
 //check if a cycle appears
 bool isItCycle (int from_candidate, int to_candidate)
 {
-    // who already beated the 'from_candidate'?
+
+    // find those who already beated the 'from_candidate'
+    // is there 'to_candidate'?
+    if (locked[to_candidate][from_candidate] == true)
+    {
+        return true;
+    }
+    
+
+
+    // then, who already beated the 'from_candidate'?
     for (int i= 0; i < candidate_count; i++)
     {
         if (locked[i][from_candidate] == true)
+        {
+            check
+        }
     }
-    // is there 'to_candidate'?
+
     // if yes
     return true;
 
