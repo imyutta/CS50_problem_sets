681a1d2b3340e2b4a9c21d962adf5342ad103179
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-15 22:53:04 +0000

    Wed, Jun 15, 2022, 10:53 PM +00:00

--- program/program.c
+++ program/program.c
@@ -7,9 +7,11 @@ int main(void)
     string s = "HI!";
     char c = s[0];
     char *p = &c;
+    int address = *p;
 
     printf("%s\n", s);
     printf("%p\n", s);
     printf("%p\n", p);
+       printf("%d\n", address);
 
 }
