7444b3ed794f6b168610b3e0c5b86d22848e8fc6
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-15 19:47:57 +0000

    Wed, Jun 15, 2022, 7:47 PM +00:00

--- program/program.c
+++ program/program.c
@@ -4,15 +4,6 @@
 
 int main(void)
 {
-  // Declare an array of four integers:
-  int myNumbers[4][4];
-//myNumbers[0][0] = 98;
-  // Add elements to it
-
-
-
-  printf("%i \n", myNumbers[0][0]);
-
 
 
 }
