fb24a40fe82c8dde01e74dc0b9661965bb531239
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-15 05:13:21 +0000

    Wed, Jun 15, 2022, 5:13 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -202,10 +202,11 @@ void print_winner(void)
     // TODO
     int winner;
     int winner_sum = 0;
+    int current_sum;
 
     for (int j = 0; j < candidate_count; j++)
     {
-        int current_sum = 0;
+        current_sum = 0;
         for ( int i = 0; i < candidate_count; i++)
         {
             if ( locked[i][j] == true)
