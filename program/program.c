1ff2e288c9d350479337840367035692afcdbd20
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-16 04:06:09 +0000

    Thu, Jun 16, 2022, 4:06 AM +00:00

--- program/program.c
+++ program/program.c
@@ -3,18 +3,14 @@
 
 int main(void)
 {
-// int k = 5;
-// printf("%d\n", k);
+int k = 5;
+printf("%d\n", k);
 
-// int *pk = &k;
-// *pk = 35;
-// printf("%d\n", *pk);
+int *pk = &k;
+*pk = 35;
+printf("%d\n", *pk);
 
 
-char k = 'b';
 
-char *pk = &k;
-*pk = 'm';
-printf("%c\n", *pk);
 
 }
