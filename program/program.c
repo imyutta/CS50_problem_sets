1aa295d3cb00b1ec990f1b91580d7b9d1faad277
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-16 05:23:48 +0000

    Thu, Jun 16, 2022, 5:23 AM +00:00

--- program/program.c
+++ program/program.c
@@ -6,7 +6,15 @@ int main(void)
 //     printf("%c\n", *pk);
 //     printf("%s\n", pk);
 //     printf("%p\n", pk);
-// 
+//
+    int numbers[] = {4, 6, 8, 2, 7, 5, 0};
 
+    printf("%i\n", *numbers);
+    printf("%i\n", *(numbers + 1));
+    printf("%i\n", *(numbers + 2));
+    printf("%i\n", *(numbers + 3));
+    printf("%i\n", *(numbers + 4));
+    printf("%i\n", *(numbers + 5));
+    printf("%i\n", *(numbers + 6));
 
 }
