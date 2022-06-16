bb3f78d2fb89507da239c12d73fbc7db0e2fa5da
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-16 04:23:13 +0000

    Thu, Jun 16, 2022, 4:23 AM +00:00

--- program/program.c
+++ program/program.c
@@ -1,17 +1,7 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-int k = 5;
-int *pk = &k;
-printf("%p\n", pk);
-
-int m = 4;
- pk = &m;
-printf("%p\n", pk);
-
-
-
-
+    char *s = "HI!";
+    printf("%p\n", s);
 }
