b917f038b277b0ade6c41c4a00c27f619303456a
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-16 05:18:37 +0000

    Thu, Jun 16, 2022, 5:18 AM +00:00

--- program/program.c
+++ program/program.c
@@ -3,12 +3,8 @@
 int main(void)
 {
     char *s = "HI!";
-    printf("%c\n", s[0]);
-    printf("%c\n", s[1]);
-    printf("%c\n", s[2]);
-    printf("%c\n", s[3]);
-    printf("%c\n", s[4]);
-    printf("%c\n", s[5]);
-    printf("%c\n", s[7]);
-    printf("%c\n", s[9]);
+    printf("%c\n", *s);
+    printf("%c\n", *(s + 1));
+
+
 }
