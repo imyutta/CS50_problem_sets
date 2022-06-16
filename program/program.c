3f47b81638c359d901a1b2b5cea07e693e9d74da
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-16 00:22:08 +0000

    Thu, Jun 16, 2022, 12:22 AM +00:00

--- program/program.c
+++ program/program.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
+    string s = "HI!";
 
-    char *p = "HI!";
-
-    printf("%c\n", *p);
-
+    char *c = "HI!";
+    char *p = &s[0];
+    
+    printf("%p\n", p);
+    printf("%p\n", s);
 }
