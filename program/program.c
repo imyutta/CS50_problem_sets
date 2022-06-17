0609dfdc938ceb03e92691de11f96c22825cd4b9
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-17 03:02:40 +0000

    Fri, Jun 17, 2022, 3:02 AM +00:00

--- program/program.c
+++ program/program.c
@@ -4,23 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-void swap(int a, int b);
 
 int main(void)
 {
-    int x = 1;
-    int y = 2;
-
-    printf("x is %i, y is %i\n", x, y);
-    swap(&x, &y);
-    printf("x is %i, y is %i\n", x, y);
-}
-
-void swap(int *a, int *b)
-{
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+    int x;
+    printf("x: ");
+    scanf("%i", &x);
 }
 
     // char *pk = "HI!";
