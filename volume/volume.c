0954a3ca55b897e1b3bfb95e70b4f126ffc99e37
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-18 02:20:52 +0000

    Sat, Jun 18, 2022, 2:20 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,7 +43,10 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     //read
     int16_t buffer;
-
+    while((buffer = fgetc(input)) != EOF)
+    {
+        printf("%int16_t", buffer);
+    }
     //update the volume
 
     //write
