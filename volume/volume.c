1a9fcd8d70e6279bd0b569af9bf7c816fdec48a9
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-18 01:44:56 +0000

    Sat, Jun 18, 2022, 1:44 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     // create an array of n bytes for the header
     uint8_t header[HEADER_SIZE];
-    
+    while(())
 
     // TODO: Read samples from input file and write updated data to output file
 
