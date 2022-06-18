efb943dbcf2b98c33944b75cd5df87121b45264b
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-18 04:37:14 +0000

    Sat, Jun 18, 2022, 4:37 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,10 +36,10 @@ int main(int argc, char *argv[])
 
     // create an array of n bytes for the header
     uint8_t header = malloc(sizeof(uint8_t)* HEADER_SIZE);
-    // if (header == NULL)
-    // {
-    //     return 1;
-    // }
+    if (header == NULL)
+    {
+        return 1;
+    }
     // if (strlen(header) > 0)
     // {
     // Copy header from input file to output file
