505cd389f4a7e6f733588c4a9a54c192704c53ff
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-18 02:01:53 +0000

    Sat, Jun 18, 2022, 2:01 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,6 +40,7 @@ int main(int argc, char *argv[])
     fread(header, sizeof(uint8_t), HEADER_SIZE, input);
 
     // TODO: Read samples from input file and write updated data to output file
+    
 
     // Close files
     fclose(input);
