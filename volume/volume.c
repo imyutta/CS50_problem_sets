431ac69db5f8761f613e5c3804d5f3acc6b7f76c
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-18 03:13:35 +0000

    Sat, Jun 18, 2022, 3:13 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,14 +36,17 @@ int main(int argc, char *argv[])
 
     // create an array of n bytes for the header
     uint8_t *header = malloc(HEADER_SIZE);
-    if (buffer == NULL)
+    if (header == NULL)
     {
         return 1;
     }
-    // Copy header from input file to output file
-    fread(header, HEADER_SIZE, 1, input);
-    fwrite(header, HEADER_SIZE, 1, output);
-    free(header);
+    if (strlen(header) > 0)
+    {
+        // Copy header from input file to output file
+        fread(header, HEADER_SIZE, 1, input);
+        fwrite(header, HEADER_SIZE, 1, output);
+        free(header);
+    }
 
     // TODO: Read samples from input file and write updated data to output file
     //read
