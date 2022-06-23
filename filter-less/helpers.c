2091c03941ffeb546a8f8f723fd249af5ffc08d0
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-23 05:09:51 +0000

    Thu, Jun 23, 2022, 5:09 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 int boundary_check(int a);
-void pixel_blur(int i, int j, RGBTRIPLE image[][], RGBTRIPLE copy[][]);
+void pixel_blur(int i, int j, *image, *copy);
 
 
 // Convert image to grayscale
@@ -100,7 +100,7 @@ int boundary_check(int a)
     }
 }
 
-void pixel_blur(int i, int j, RGBTRIPLE image[height][width], RGBTRIPLE copy[height][width])
+void pixel_blur(int i, int j, *image, *copy)
 {
     int blurRed = 0;
     int blurGreen = 0;
