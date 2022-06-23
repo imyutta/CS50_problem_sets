eb82b5f4204e7f3bdfb3305bf253ffbfb51ffd31
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-23 06:15:38 +0000

    Thu, Jun 23, 2022, 6:15 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -13,11 +13,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            RGBTRIPLE pixel = image[i][j];
+            average = round((pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed) / 3.0);
 
-            image[i][j].rgbtBlue = average;
-            image[i][j].rgbtGreen = average;
-            image[i][j].rgbtRed = average;
+            pixel.rgbtBlue = average;
+            pixel.rgbtGreen = average;
+            pixel.rgbtRed = average;
         }
     }
     return;
