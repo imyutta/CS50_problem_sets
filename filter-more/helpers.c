baeea062427ede32bb9a430d05c150afb613367f
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-23 23:24:48 +0000

    Thu, Jun 23, 2022, 11:24 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,12 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int ave
+    int average;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE 
+            RGBTRIPLE pixel = image[i][j];
+            average = round((pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed) / 3.0);
+
+            pixel.rgbtBlue = average;
+            pixel.rgbtGreen = average;
+            pixel.rgbtRed = average;
         }
     }
     return;
@@ -17,12 +22,73 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE pixelHolder;
+    // int last_j;
+    int lastElement = width - 1;
+
+    int last_j = floor(width / 2) - 1;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j <= last_j; j++)
+        {
+            pixelHolder = image[i][j];
+            image[i][j] = image[i][lastElement - j];
+            image[i][lastElement - j] = pixelHolder;
+
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    int blurRed;
+    int blurGreen;
+    int blurBlue;
+    float counter;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //pixel_blur(i, j, RGBTRIPLE image[height][width], RGBTRIPLE copy[height][width]);
+            blurRed = 0;
+            blurGreen = 0;
+            blurBlue = 0;
+            counter = 0;
+            for (int row = i - 1, last_row = i + 1; row <= last_row; row++)
+            {
+                if (row >= 0 && row < height)
+                {
+                    for (int column = j - 1, last_column = j + 1; column <= last_column; column++)
+                    {
+                        if (column >= 0 && column < width)
+                        {
+                            blurRed += copy[row][column].rgbtRed;
+                            blurGreen += copy[row][column].rgbtGreen;
+                            blurBlue += copy[row][column].rgbtBlue;
+                            counter++;
+
+                        }
+                    }
+                }
+            }
+            image[i][j].rgbtRed = round(blurRed / counter);
+            image[i][j].rgbtGreen = round(blurGreen / counter);
+            image[i][j].rgbtBlue = round(blurBlue / counter);
+        }
+    }
     return;
 }
 
