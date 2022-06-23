62eac5b166f0b71b5f6d4d0ea0ce92a52591bd41
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-23 01:02:13 +0000

    Thu, Jun 23, 2022, 1:02 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -79,6 +79,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             copy[i][j] = image[i][j];
+            
         }
     }
     // int blurRed, blurGreen, blurBlue;
