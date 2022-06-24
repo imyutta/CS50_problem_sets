5962629e1e356d71718b8b871f784c3f223f626c
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-24 05:19:49 +0000

    Fri, Jun 24, 2022, 5:19 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -152,8 +152,8 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             pixel[8].rgbtGreen = copy[i + 1][j + 1].rgbtGreen;
 
 
-            g_X_RED = pixel[1].rgbtRed * (-1) + pixel[3].rgbtRed * 1 + pixel[4].rgbtRed * (-2) + pixel[6].rgbtRed * 2 + pixel[7].rgbtRed * (-1) + pixel[9].rgbtRed * 1;
-            g_Y_RED = pixel[1].rgbtRed * (-1) + pixel[2].rgbtRed * (-2) + pixel[3].rgbtRed * (-1) + pixel[7].rgbtRed * 1 + pixel[8].rgbtRed * (2) + pixel[9].rgbtRed * 1;
+            g_X_RED = pixel[0].rgbtRed * (-1) + pixel[2].rgbtRed * 1 + pixel[3].rgbtRed * (-2) + pixel[5].rgbtRed * 2 + pixel[6].rgbtRed * (-1) + pixel[8].rgbtRed * 1;
+            g_Y_RED = pixel[0].rgbtRed * (-1) + pixel[2].rgbtRed * (-2) + pixel[3].rgbtRed * (-1) + pixel[7].rgbtRed * 1 + pixel[8].rgbtRed * (2) + pixel[9].rgbtRed * 1;
             g_SUM_RED = sqrt(pow(g_X_RED, 2) + pow(g_Y_RED, 2));
 
             g_X_BLUE = pixel[1].rgbtBlue * (-1) + pixel[3].rgbtBlue * 1 + pixel[4].rgbtBlue * (-2) + pixel[6].rgbtBlue * 2 + pixel[7].rgbtBlue * (-1) + pixel[9].rgbtBlue * 1;
