06a4b7d7825632ef451bc01eb567b236ebda7299
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-24 03:31:28 +0000

    Fri, Jun 24, 2022, 3:31 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -129,6 +129,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
             pixel = image[i][j];
 
+            
             for (int row = i - 1, last_row = i + 1; row <= last_row; row++)
             {
                 if (row >= 0 && row < height)
