c6bdcc1bb67f07c602e6153d563c677413ea92be
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-30 22:31:26 +0000

    Thu, Jun 30, 2022, 10:31 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,28 +24,29 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-//copy of the file
+
     //allocate memory for the file
     typedef uint8_t BYTE;
     int BLOCK_SIZE = 512;
-    BYTE* buffer = malloc(sizeof(BYTE));
+    BYTE* buffer = malloc(sizeof(BYTE) * BLOCK_SIZE);
     if (buffer == 0)
     {
-        printf("Could not open the file");
+        printf("Could not crete a buffer to copy the file");
         fclose(file);
         return 1;
     }
-    // create a copy of the memory card
-    while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
-    {
-    }
-
+// while creating a copy of the memory card
 //look for the beginning of a JPEG file
 // each time you find a signature
     //open a new file
     //fill this file with bytes from the memory card
     //till you meet another signature
     // read 512 at a time
+    while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
+    {
+    }
+
+
     char* jpeg_name = malloc(4 * sizeof(char));
     if (jpeg_name == NULL)
     {
