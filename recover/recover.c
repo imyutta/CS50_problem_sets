90c4913e62371ad59cde413a0a172189db71c0d6
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-30 23:00:56 +0000

    Thu, Jun 30, 2022, 11:00 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
         //open a new jpeg file
         //fill this jpeg file with bytes from the memory card
         //close when you meet another signature
-
+    FILE* img = ma
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
