69109c3843ba5aed8b1a51097767aa3fc5d4f78c
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-30 19:53:49 +0000

    Thu, Jun 30, 2022, 7:53 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
     // read 512 at a time
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
-
+        sprintf
     }
 
 
