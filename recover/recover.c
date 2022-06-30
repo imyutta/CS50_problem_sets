995e0480ad9468a7579f42cc13439ffb5cba0b48
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-30 21:09:57 +0000

    Thu, Jun 30, 2022, 9:09 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -66,7 +66,11 @@ int main(int argc, char *argv[])
         {
             sprintf(jpeg_name, "%03i.jpg", i);
             FILE* img = fopen(jpeg_name, "w");
-            fwrite(buffer, 1, BLOCK_SIZE, img);
+            do
+            {
+                fwrite(buffer, 1, BLOCK_SIZE, img);
+            }
+            while (buffer[n] != 0xff)
         }
     }
 
