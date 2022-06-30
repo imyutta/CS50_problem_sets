2e6c35ab45cb5b5bdb2100d541457d66fd9afe2b
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-30 17:43:27 +0000

    Thu, Jun 30, 2022, 5:43 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,12 +7,20 @@ int main(int argc, char *argv[])
     //check that there is only 1 command-line argument
     if (argc != 2)
     {
+        printf("there must be only one argument")
         return 1;
     }
 
-    //check that the command-line argument is the name of the forensic image
+    //command-line argument is the name of the forensic image
     //from which to recover JPEGs
-    if 
+
+    //open the file
+    FILE *file = fopen(argv[1], "r")
+    if (file == NULL)
+    {
+        printf("Could not open the file");
+        return 2;
+    }
 
 
 
