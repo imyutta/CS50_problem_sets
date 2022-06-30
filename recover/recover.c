f53d2a7259681ff4eb73783bc39d26a55f0f7f87
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-30 19:57:50 +0000

    Thu, Jun 30, 2022, 7:57 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
         string jpeg_name;
-        sprintf(jpeg_name, "%03i.jpg", )
+        sprintf(jpeg_name, "%03i.jpg", i)
     }
 
 
