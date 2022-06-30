bde4f32ad3e9336f7744a0f44b41eb86ec0b3b1b
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-30 23:13:53 +0000

    Thu, Jun 30, 2022, 11:13 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     //check that there is only 1 command-line argument
     if (argc != 2)
     {
-        printf("there must be only one argument");
+        printf("there must be only one argument\n");
         return 1;
     }
 
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
-        printf("Could not open the file");
+        printf("Could not open the file\n");
         return 1;
     }
 
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     BYTE* buffer = malloc(sizeof(BYTE) * BLOCK_SIZE);
     if (buffer == 0)
     {
-        printf("Could not crete a buffer to copy the file");
+        printf("Could not create a buffer to copy the file\n");
         fclose(file);
         return 1;
     }
@@ -44,6 +44,7 @@ int main(int argc, char *argv[])
     {
         fclose(file);
         free(buffer);
+        printf("Could not create a new jpeg name\n");
         return 1;
     }
     if (sizeof(jpeg_name <= 0))
@@ -51,6 +52,7 @@ int main(int argc, char *argv[])
         fclose(file);
         free(buffer);
         free(jpeg_name);
+        printf("size of the jpeg_name is equal to 0\n");
         return 1;
     }
 
@@ -67,6 +69,7 @@ int main(int argc, char *argv[])
         fclose(file);
         free(buffer);
         free(jpeg_name);
+        printf("Could not create a pointer to a new image\n");
         return 1;
     }
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
@@ -94,7 +97,6 @@ int main(int argc, char *argv[])
         {
             fwrite(buffer, 1, BLOCK_SIZE, img);
         }
-
 
     }
 
