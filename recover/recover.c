936feca0405a1993cd308dd198ae2b1167ba8532
Yutta <105465153+imyutta@users.noreply.github.com>
2022-06-30 20:08:05 +0000

    Thu, Jun 30, 2022, 8:08 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -51,8 +51,8 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            sprintf(jpeg_name, "%03i.jpg", i)
-            FILE* 
+            sprintf(jpeg_name, "%03i.jpg", i);
+            FILE* img = fopen(jpeg_name, "w");
         }
     }
 
