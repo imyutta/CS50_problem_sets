4948379f3deb206cbb756cdd3e3dac736a362d63
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-01 22:35:18 +0000

    Fri, Jul 1, 2022, 10:35 PM +00:00

--- program/argc.c
+++ program/argc.c
@@ -1,16 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[])
+int main()
 {
-    if (argc != 2)
-    {
-        printf("missing command-line argument\n");
-        return 1;
-    }
-        printf("hello, %s\n", argv[1]);
-        return 1;
-    
 
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
 }
 
