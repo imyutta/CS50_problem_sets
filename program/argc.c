435d03c14fff66ff93c75fd00b6d5750b10552a5
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-01 23:02:27 +0000

    Fri, Jul 1, 2022, 11:02 PM +00:00

--- program/argc.c
+++ program/argc.c
@@ -1,14 +1,34 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main()
-{
+// int main()
+// {
 
-typedef struct node
+// typedef struct node
+// {
+//     int number;
+//     struct node *next;
+// }
+// node;
+// }
+
+int main(void)
 {
-    int number;
-    struct node *next;
-}
-node;
+    int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+
+    
 }
 
