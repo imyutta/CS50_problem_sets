ca59c6a821e8ab13f24bbcb7d926c50fffeeccde
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-03 04:24:09 +0000

    Sun, Jul 3, 2022, 4:24 AM +00:00

--- program/argc.c
+++ program/argc.c
@@ -17,35 +17,6 @@
 int main(void)
 {
     int p = 50;
-    int *ptr = &p;
-    int *list = malloc(3 * sizeof(int));
-    if (list == NULL)
-    {
-        return 1;
-    }
 
-    list[0] = p;
-    list[1] = 2;
-    list[2] = 3;
-    ptr = list;
-
-
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-
-    free(list);
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-
-    // list = malloc(4 * sizeof(int));
-    // list[3] = 4;
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     printf("%i\n", list[i]);
-    // }
 }
 
