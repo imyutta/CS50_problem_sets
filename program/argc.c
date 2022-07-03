9245a40b2fe1e31ae78e56b27666246e73e3e117
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-03 21:01:58 +0000

    Sun, Jul 3, 2022, 9:01 PM +00:00

--- program/argc.c
+++ program/argc.c
@@ -3,56 +3,21 @@
 #include <stdlib.h>
 
 
-// int main()
-// {
-
-// typedef struct node
-// {
-//     int number;
-//     struct node *next;
-// }
-// node;
-
-// node *list = NULL;
-// node *n = malloc(sizeof(node));
-// if (n != NULL)
-// {
-//     (*n).number = 1;
-//     n->next = NULL;
-// }
-// list = n;
-// free(list);
-// }
-
-int main(void)
+int main()
 {
-    // Dynamically allocate an array of size 3
-    int *list = malloc(3 * sizeof(int));
-    if (list == NULL)
-    {
-        return 1;
-    }
 
-    // Assign three numbers to that array
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
 
-    // Time passes
-
-    // Resize old array to be of size 4
-    int *tmp = realloc(list, 4 * sizeof(int));
-    if (tmp == NULL)
-    {
-        free(list);
-        return 1;
-    }
-    tmp[3] = 4;
-
-    list = tmp;
-
-    free(list);
-
+n = malloc(sizeof(node));
+if ( n != NULL)
+{
+    n->number = 2;
+    n->next = NULL;
 }
 
-
+}
