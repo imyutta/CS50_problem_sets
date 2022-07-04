44922835f86f3745502d69bd250349006929e59e
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-04 19:32:43 +0000

    Mon, Jul 4, 2022, 7:32 PM +00:00

--- program/program5.c
+++ program/program5.c
@@ -1,15 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
 
 typedef struct node
 {
-    char word[LONGEST_WORD + 1];
-    struct node *next;
-}
-node;
-
-node *hash_table[NUMBER_OF_BUCKETS];
+    bool is_word;
+    struct node *children[5]
 
 int main()
 {
