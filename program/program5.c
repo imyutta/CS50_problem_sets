502cf340d851e87e821ebac97ed0c598e60a27a6
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-05 19:14:46 +0000

    Tue, Jul 5, 2022, 7:14 PM +00:00

--- program/program5.c
+++ program/program5.c
@@ -4,12 +4,16 @@
 #include <stdint.h>
 
 
-typedef struct node
+int main(void)
 {
-    bool is_word;
-    struct node *children[5]
-}
-int main()
-{
+    int list[3];
 
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", list[i]);
+    }
 }
