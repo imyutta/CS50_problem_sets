36bce0dcee9d47ca972538cd19b1ee89378d7886
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-07 03:20:43 +0000

    Thu, Jul 7, 2022, 3:20 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -54,7 +54,7 @@ person *create_family(int generations)
         n->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        n->alleles
+        n->alleles[0] = 
     }
 
     // If there are no generations left to create
