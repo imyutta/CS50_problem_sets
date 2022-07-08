b2573cdaa1cc5d15fb4c3d411262720dbe85882f
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-08 02:57:49 +0000

    Fri, Jul 8, 2022, 2:57 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -94,9 +94,8 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p->parents[0] == NULL && p->parents[1] == NULL)
+    if (p == NULL)
     {
-        free(p);
         return;
     }
     // TODO: Free parents recursively
