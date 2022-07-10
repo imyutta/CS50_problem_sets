ee4e13981d515465fd7c7def0b50d1bd1e8d9644
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-10 20:23:48 +0000

    Sun, Jul 10, 2022, 8:23 PM +00:00

--- program/argc.c
+++ program/argc.c
@@ -3,48 +3,11 @@
 
 int main(void)
 {
-    int height, line, p, space;
-    int m, n, k;
+    int scores[3];
 
-// Ask user for a positive number from 1 to 8
-    do
-    {
-        height = get_int("Height: \n");
-    }
-    while (height < 1 || height > 8);
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
 
-//the amount of lines will be
-    for (line = 0; line < height; line++)
-    {
-
-//printing first dots "     "
-        for (space = 0; space < (height - line - 1); space++)
-        {
-            printf(" ");
-        }
-//printing first #'s
-        // while (p > 0)
-        // {
-        //     printf("#");
-        //     p--;
-        // }
-        for (p = line + 1; p > 0; p--)
-        {
-            printf("#");
-        }
-//printing second dots ".."
-        printf("  ");
-
-//printing second #'s
-        // while (z > 0)
-        // {
-        //     printf("#");
-        //     z--;
-        // }
-        for (p = line + 1; p > 0; p--)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
