8e6be1c4d61fed31f127e7e81876a4f9235c89d4
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-13 01:23:48 +0000

    Wed, Jul 13, 2022, 1:23 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -135,7 +135,7 @@ bool load(const char *dictionary)
         dictionary_size ++;
     }
 
-    printf("table[i]: %s\n", table[18]->word);
+    printf("table[i]: %s\n", table[235]->word);
 
     return true;
 }
