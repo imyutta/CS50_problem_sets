9ede6232b14a1ca759f51cf3810ca476de5d535f
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-14 23:34:01 +0000

    Thu, Jul 14, 2022, 11:34 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,12 +1,21 @@
-#
+
+from cs50 import get_float
 # ask user how much change he needs
 
 def main():
     change = get_change()
+    print(change)
 
 
 
 def get_change():
-    n = get_float
+    while True:
+        try:
+            n = get_float("Change: ")
+            if n >= 0:
+                break
+        except ValueError:
+            print("Please provide a non-negative float number")
+    return n
 
 main()
