9e1af538617c45c71e08e9c3bcfedfab274be247
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-14 23:25:06 +0000

    Thu, Jul 14, 2022, 11:25 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,5 +1,11 @@
-quantity = 3
-itemno = 567
-price = 49.95
-myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
-print(myorder.format(quantity, itemno, price))
+#
+# ask user how much change he needs
+
+def main():
+    change = get_change()
+
+
+
+def get_change():
+    
+main()
