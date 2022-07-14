1b8aa91d621b4b0653ff645c5f9cb3e75fda7add
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-14 19:55:39 +0000

    Thu, Jul 14, 2022, 7:55 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,18 +1,13 @@
 
-# while True:
-#     height = input("Height: ")
-#     if type(height) == int and height > 0 and height < 8:
-#         break
+while True:
+    height = input("Height: ")
+    if type(height) == int:
+    # and height > 0 and height < 8:
+        break
 
-# for i in range(height):
-#     for j in range(height - i - 1):
-#         print("_")
-#     for k in range(i):
-#         print("#")
+for i in range(height):
+    for j in range(height - i - 1):
+        print("_")
+    for k in range(i):
+        print("#")
 
-from cs50 import get_int
-
-n = get_int("Height: ")
-
-for i in range(n):
-    print("#")
