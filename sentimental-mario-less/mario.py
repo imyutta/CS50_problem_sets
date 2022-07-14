11b2ba04ddbf4045ba286e94feb6e211e6860bb8
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-14 19:45:04 +0000

    Thu, Jul 14, 2022, 7:45 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,13 +1,8 @@
-# height = input("Height: ")
+height = int()
+height = input("Height: ")
 
-# for i in range(height):
-#     for j in range(height - i - 1):
-#         print("_")
-#     for k in range(i):
-#         print("#")
-
-for x in range(6):
-  if x == 3: break
-  print(x)
-else:
-  print("Finally finished!")
+for i in range(height):
+    for j in range(height - i - 1):
+        print("_")
+    for k in range(i):
+        print("#")
