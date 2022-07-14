c0e17d4f12c27c54b49769a0d1f561e757351148
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-14 21:07:00 +0000

    Thu, Jul 14, 2022, 9:07 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,26 @@
 # TODO
+
+def main():
+    height = get_height()
+
+    for i in range(height):
+        spaces = height - i - 1
+        sharps = i + 1
+        print(" " * spaces, end="")
+        print("#" * sharps, end="")
+        print()
+
+
+
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0 and n < 9:
+                break
+        except ValueError:
+            print("it should be an integer between 1 and 8 inclusive")
+    return n
+
+
+main()
