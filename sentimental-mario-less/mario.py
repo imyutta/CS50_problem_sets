6a1f76e8a8192397976b971cb76e0b137212d90a
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-14 20:17:53 +0000

    Thu, Jul 14, 2022, 8:17 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,18 +1,14 @@
 
 while True:
-  try:
-    height = int(input("Height: "))
-    if type(height) == int and height > 0 and height < 8:
-        break
-    else:
-        print("non valid integer")
+    try:
+        height = int(input("Height: "))
+        if type(height) == int and height > 0 and height < 8:
+            break
+        else:
+            print("non valid integer")
 
 for i in range(height):
     for j in range(height - i - 1):
         print("_")
     for k in range(i):
         print("#")
-
-
-
-
