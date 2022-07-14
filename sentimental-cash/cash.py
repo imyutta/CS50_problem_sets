a2a9356d59176fbdaac32ac05e5c23c3baea4b77
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-14 22:54:22 +0000

    Thu, Jul 14, 2022, 10:54 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,5 +1,3 @@
-a = """Lorem ipsum dolor sit amet,
-consectetur adipiscing elit,
-sed do eiusmod tempor incididunt
-ut labore et dolore magna aliqua.'''
-print(a)
+a = "Hello"
+a[1] = B
+print(a[1])
