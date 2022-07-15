7bb8b00aa00dcdaf9bf288e010c653b06aa6d818
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-15 00:23:57 +0000

    Fri, Jul 15, 2022, 12:23 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -13,7 +13,7 @@ def main():
     # calculate quarters, dimes, nickels, pennies
     # calculate sum of coins
     for i in [25, 10, 5, 1]:
-        quarters = int(change / i)
+        quarters = change // i
         change = change % i
         coins += quarters
     print(int(coins))
@@ -22,12 +22,12 @@ def main():
 # prompt user for a non-negative float number
 def get_change():
     while True:
-        try:
+        # try:
             n = get_float("Change: ")
             if n >= 0:
                 break
-        except ValueError:
-            print("Please provide a non-negative float number")
+        # except ValueError:
+        #     print("Please provide a non-negative float number")
     return n
 
 
