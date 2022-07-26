892274f59710a6a12a914178f46aa1f494b80540
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-26 03:55:54 +0000

    Tue, Jul 26, 2022, 3:55 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -16,7 +16,7 @@ if card_number % 2 == 0:
     # multiply every 2nd digit (counting from the 2nd from the end) by 2
     for i in card_number:
         second_digit = int(i) * 2
-        second_digits_sum += seond_digit % 10
+        second_digits_sum += (seсond_digit % 10) + (seсond_digit / 10)
 
 
 # add those mult_digits2 together (not the numbers, but their digits)
