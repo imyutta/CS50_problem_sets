3af7215f05f52f7d49d590ca98002cd91ad7622d
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-26 03:32:37 +0000

    Tue, Jul 26, 2022, 3:32 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -7,15 +7,16 @@ card_number = input("Number: ")
 # name second_digit means that we start counting from the end
 # name first_digit means that we start counting from the end
 
-second_digits = 0
+second_digit = 0
 second_digits_sum = 0
-first_digits = 0
+first_digit = 0
 first_digits_sum = 0
 
 if card_number % 2 == 0:
     # multiply every 2nd digit (counting from the 2nd from the end) by 2
     for i in card_number:
-        int(i) * 2
+        second_digits = int(i) * 2
+        second_digits_sum += 
 
 
 # add those mult_digits2 together (not the numbers, but their digits)
