1addd217666293fe79be0f79e4144fbbbe00dc15
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-26 02:13:42 +0000

    Tue, Jul 26, 2022, 2:13 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,6 +5,7 @@ from cs50 import get_int
 card_number = get_int("Number: ")
 
 # multiply every 2nd digit (counting from the 2nd from the end) by 2
+for
 
 # add those mult_digits2 together (not the numbers, but their digits)
 
