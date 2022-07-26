e911ae8e92a41b6239042e4d183d925c41b52fe9
Yutta <105465153+imyutta@users.noreply.github.com>
2022-07-26 02:19:42 +0000

    Tue, Jul 26, 2022, 2:19 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,8 +4,18 @@ from cs50 import get_int
 # promt user for a card number
 card_number = get_int("Number: ")
 
+# name second_digit means that we start counting from the end
+# name first_digit means that we start counting from the end
+
+second_digit = 0
+second_digit_sum = 0
+first_digit = 0
+first_digit_sum = 0
+
 # multiply every 2nd digit (counting from the 2nd from the end) by 2
-for 
+for i in range (card_number)
+    
+
 
 # add those mult_digits2 together (not the numbers, but their digits)
 
