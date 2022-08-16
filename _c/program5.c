735f1dbee5eed2c2dfcf6350fff9ebafcd2a72a0
Yutta <105465153+imyutta@users.noreply.github.com>
2022-08-16 00:14:20 +0000

    ddd, Aug 8/16/2022, YYYY, 12:14 A +0

--- _c/program5.c
+++ _c/program5.c
@@ -1,73 +1,49 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_cents(void);
-int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickels(int cents);
-int calculate_pennies(int cents);
-
 int main(void)
 {
-    // Ask how many cents the customer is owed
-    int cents = get_cents();
+    int height, line, p, space;
 
-    // Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
-
-    // Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
-
-    // Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
-
-    // Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
-
-    // Sum coins
-    int coins = quarters + dimes + nickels + pennies;
-
-    // Print total number of coins to give the customer
-    printf("%i\n", coins);
-}
-
-int get_cents()
-{
-    // Ask how many cents are owed?
-    int number_of_cents;
+// Ask user for a positive number from 1 to 8
     do
     {
-        number_of_cents = get_int("How many cents are owed? ");
+        height = get_int("Height: \n");
     }
-    while (number_of_cents < 0);
-    return number_of_cents;
-}
+    while (height < 1 || height > 8);
 
+//the amount of lines will be
+    for (line = 0; line < height; line++)
+    {
 
-int calculate_quarters(int cents)
-{
-    // Calculate quarters
-    return cents / 25;
-}
+//printing first dots "     "
+        for (space = 0; space < (height - line - 1); space++)
+        {
+            printf(" ");
+        }
+//printing first #'s
+        // while (p > 0)
+        // {
+        //     printf("#");
+        //     p--;
+        // }
+        for (p = line + 1; p > 0; p--)
+        {
+            printf("#");
+        }
+//printing second dots ".."
+        printf("  ");
 
-int calculate_dimes(int cents)
-{
-    // calculate dimes
-    return cents / 10;
-}
-
-int calculate_nickels(int cents)
-{
-    // calculate nickels
-    return cents / 5;
-}
-
-int calculate_pennies(int cents)
-{
-    // calculate pennies
-    return cents;
+//printing second #'s
+        // while (z > 0)
+        // {
+        //     printf("#");
+        //     z--;
+        // }
+        for (p = line + 1; p > 0; p--)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
