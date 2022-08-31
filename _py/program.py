a40513789788d00567d368d66d1f29fd4ad968d9
Yutta <105465153+imyutta@users.noreply.github.com>
2022-08-31 23:04:16 +0000

    ddd, Aug 8/31/2022, YYYY, 11:04 A +0

--- _py/program.py
+++ _py/program.py
@@ -1,15 +1,3 @@
+from cs50 import get_int
 
-try:
-    x = int(input("x: "))
-except ValueError:
-    print("That is not an int!")
-    exit()
-try:
-    y = int(input("y: "))
-except ValueError:
-    print("Not an int")
-    exit()
-
-z = x / y
-
-print(f"{z:.50f}")
+n = get_int
