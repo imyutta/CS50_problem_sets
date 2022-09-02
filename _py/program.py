72f4eca5c6af0bdb31f9ac1f43b7d984f491c65c
Yutta <105465153+imyutta@users.noreply.github.com>
2022-09-02 20:10:08 +0000

    ddd, Sep 9/2/2022, YYYY, 8:10 A +0

--- _py/program.py
+++ _py/program.py
@@ -1,9 +1,2 @@
-from cs50 import get_string
-
-s = get_string("agree? ").lower()
-
-
-if s in ["y", "yes"]:
-    print("Agreed")
-elif s in ["n", "no"]:
-    print("Not agreed")
+for i in range(3):
+    print("mew")
