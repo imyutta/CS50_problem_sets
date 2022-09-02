0e347f980322a32494a72288813a3cd196a840b9
Yutta <105465153+imyutta@users.noreply.github.com>
2022-09-02 22:00:28 +0000

    ddd, Sep 9/2/2022, YYYY, 10:00 A +0

--- _py/program.py
+++ _py/program.py
@@ -3,6 +3,5 @@ from cs50 import get_string
 before = get_string("before: ")
 print("After: ", end = "")
 
-for c in before:
-    print(c.upper(), end="")
-print()
+after = before.upper()
+print(after)
