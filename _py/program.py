eef9706290ec1f7e06fbf69d6c59f2823bd41bd1
Yutta <105465153+imyutta@users.noreply.github.com>
2022-09-03 01:29:05 +0000

    ddd, Sep 9/3/2022, YYYY, 1:29 A +0

--- _py/program.py
+++ _py/program.py
@@ -4,4 +4,9 @@ from cs50 import get_string
 file = open("phonebook.csv, "a")
 
 name = get_string("Name: ")
-number
+number = get_string("Name: ")
+
+writer = csv.writer(file)
+writer.writerow([name, number])
+
+file.close()
