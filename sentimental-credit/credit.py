e51050172e8099a2c3420668147d3cfc6ee14d5d
Yutta <105465153+imyutta@users.noreply.github.com>
2022-09-07 21:29:56 +0000

    ddd, Sep 9/7/2022, YYYY, 9:29 A +0

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,8 +5,8 @@ from cs50 import get_int
 card_number = get_int("Number: ")
 
 if validation_check(card_number) == false:
-    print(Invalid number)
-    break
+    print(invalid number)
+    return 1
 
 
 def validation_check(x):
@@ -16,9 +16,13 @@ def validation_check(x):
             multiplied_digit = i * 2
             sum += multiplied_digit % 10
             sum += multiplied_digit // 10
-        for i in range(1, x, 2)
+        for i in range(1, x, 2):
             sum += i
-        
+        if sum % 10 == 0:
+            return true
+        else:
+            return false
+
 
 
 # underline every other digit, starting with the number’s second-to-last digit:
@@ -31,8 +35,6 @@ every_other_digit = 0
 # add that sum to the sum of the digits that weren’t multiplied by 2 (starting from the end):
 
 # if the last digit is 0 - the number is ligit
-    return true
-    return false
 
 
 
@@ -40,24 +42,3 @@ every_other_digit = 0
 
 
 
-# name "second_digit" means that we begun counting from the end
-# name first_digit means that we start counting from the end
-
-
-second_digits_sum = 0
-first_digit = 0
-first_digits_sum = 0
-
-if len(card_number) % 2 == 0:
-    # multiply every 2nd digit (counting from the 2nd from the end) by 2
-    for i in card_number:
-        second_digit = int(i) * 2
-        second_digits_sum += (second_digit % 10) + (second_digit / 10)
-        print(second_digits_sum)
-else:
-    print("not even")
-
-
-# add those mult_digits2 together (not the numbers, but their digits)
-
-# find sum of every 2nd digit (counting from the 1st!!! from the end)
