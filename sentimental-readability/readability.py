59106810d2d6a99d13d4e42ed80a4aabd2526585
Yutta <105465153+imyutta@users.noreply.github.com>
2022-09-08 20:25:11 +0000

    ddd, Sep 9/8/2022, YYYY, 8:25 A +0

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,9 +1,8 @@
 # TODO
-
 from cs50 import get_string
-import sys
 
-#  Coleman-Liau index is computed as 0.0588 * L - 0.296 * S - 15.8
+def main():
+
 
 # prompt user for a text
     text = get_string("Text: ")
@@ -14,7 +13,7 @@ import sys
     for i in text:
         if i.isspace():
             words += 1
-    print(words)
+    print(f{words + "words"})
 
 
 # find the amount of letters
@@ -23,7 +22,7 @@ import sys
     for i in text:
         if i.isalpha():
             letters += 1
-    print(letters)
+    print(letters + "letters")
 
 # find the amount of sentences
 # any occurrence of a period, exclamation point, or question mark indicates the end of a sentence.
@@ -31,12 +30,17 @@ import sys
     for i in text:
         if i == "." or i == "!" or i == "?":
             sentences += 1
-    print(sentences)
+    print(sentences + "sentences")
 
 
-
+#  Coleman-Liau index is computed as 0.0588 * L - 0.296 * S - 15.8
 # find L (letters_average) an average amount of letters per 100 words
     letters_average = (letters / words) * 100
 
 # find S (sentences_average) the average number of sentences per 100 words in the text
     sentences_average = (sentences / words) * 100
+
+# compute Coleman-Liau index
+    index = 0.0588 * letters_average - 0.296 * sentences_average - 15.8
+    print(index + "index")
+main()
