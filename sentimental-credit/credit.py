158c91b7674a626aef24a5a23b3b2b20aa7d69ca
Yutta <105465153+imyutta@users.noreply.github.com>
2022-09-08 00:30:09 +0000

    ddd, Sep 9/8/2022, YYYY, 12:30 A +0

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -15,36 +15,17 @@ def main():
 
 
 def validation_check(x):
-# how many digits does the card number have?
-    length = len(x)
-
-# is there an even amount of digits?
-# if YES, we will start to iterate from the first digit.
-#   index_1 = 0
-# if NO, we start to iterate from the second digit.
-#   index_1 = 1
-    # if length % 2 == 0:
-    #     index_1 = 0
-    #     index_2 = 1
-    # else:
-    #     index_1 = 1
-    #     index_2 = 0
-
 # we take every other digit
 # multiply each of the digits by 2
 # add those products’ digits (i.e., not the products themselves) together
 # add that sum to the sum of the remaining digits (that weren’t multiplied by 2)
     sum = 0
     for i in x[-2::-2]:
-        print(i, end="")
-    print()
-
-#     for i in range(index_1, length, 2):
-#         multiplied_digit = x[i] * 2
-#         sum += multiplied_digit % 10
-#         sum += multiplied_digit // 10
-#     for i in range(index_2, length, 2):
-#         sum += x[i]
+        multiplied_digit = int(i) * 2
+        sum += multiplied_digit % 10
+        sum += multiplied_digit // 10
+    for i in x[-1::-2]:
+        sum += x[i]
 
 
 # # check if the last digit is 0
