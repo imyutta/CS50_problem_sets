26e8f92bc97d56954e4bad405c706e824e0ceaec
Yutta <105465153+imyutta@users.noreply.github.com>
2022-09-08 20:06:12 +0000

    ddd, Sep 9/8/2022, YYYY, 8:06 A +0

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -8,14 +8,27 @@ import sys
 # prompt user for a text
     text = get_string("Text: ")
 
-# find L (lettera_average) an average amount of letters per 100 words
+# find the amount of words
+# any sequence of characters separated by spaces should count as a word
+    words = 0
+    for i in text:
+        
+
+
+# find the amount of letters
 # a letter is any lowercase character from a to z or any uppercase character from A to Z
     letters = 0
     for i in text:
         if i.isalpha():
             letters += 1
+
+# find the amount of sentences
+# any occurrence of a period, exclamation point, or question mark indicates the end of a sentence.
+
+
+# find L (letters_average) an average amount of letters per 100 words
     letters_average = letters / 100
     print(letters_average)
 
-# find S - the average number of sentences per 100 words in the text.
+# find S (sentences_average) the average number of sentences per 100 words in the text.
 
