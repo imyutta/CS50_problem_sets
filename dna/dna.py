7268aa517b4cf7d1c37cdfde1b1f32db5e9c7e56
Yutta <105465153+imyutta@users.noreply.github.com>
2022-09-16 22:10:57 +0000

    ddd, Sep 9/16/2022, YYYY, 10:10 A +0

--- dna/dna.py
+++ dna/dna.py
@@ -9,10 +9,13 @@ def main():
         sys.exit("Usage: enter two command-line arguments")
 
     # TODO: Read database file into a variable
+    database = []
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         for x in reader:
-            
+            database.append(x)
+        print(database)
+
 
     # TODO: Read DNA sequence file into a variable
 
