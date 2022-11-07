de7f2be5101d2940e84c0046be3bf18a6dee3958
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-07 22:53:02 +0000

    ddd, Nov 11/7/2022, YYYY, 10:53 A +0

--- _py/python.py
+++ _py/python.py
@@ -1,16 +1,4 @@
 import csv
 
-titles = {}
-
 with open("favorites.csv", "r") as file:
-    reader = csv.DictReader(file)
-
-    for row in reader:
-        title = row["title"].strip().upper()
-        if not title in titles:
-            titles[title] = 0
-        titles[title] += 1
-
-
-for title in sorted(titles, key=lambda title: titles[title], reverse = True):
-    print(title, titles[title])
+    reader 
