095d5c2183c0a01b0c492ad83cf487892ffda15b
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-09 22:11:59 +0000

    ddd, Nov 11/9/2022, YYYY, 10:11 A +0

--- _py/python.py
+++ _py/python.py
@@ -1,7 +1,7 @@
 import csv
 
 with open("favorites.csv", "r") as file:
-    reader = csv.reader(file)
-    next(reader)
+    reader = csv.DictReader(file)
+
     for row in reader:
         print(row[1])
