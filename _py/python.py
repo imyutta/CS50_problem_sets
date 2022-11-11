3263494cae02997504f481b3e6800e3a2774be6d
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-11 00:40:22 +0000

    ddd, Nov 11/11/2022, YYYY, 12:40 A +0

--- _py/python.py
+++ _py/python.py
@@ -8,3 +8,6 @@ with open("favorites.csv", "r") as file:
     for row in reader:
         if not row["title"] in titles:
             titles.append(row["title"])
+
+for title in titles:
+    print(title)
