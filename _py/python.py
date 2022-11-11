1bca8647afa34b224caf4b297538b0c34bae1ebf
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-11 20:51:10 +0000

    ddd, Nov 11/11/2022, YYYY, 8:51 A +0

--- _py/python.py
+++ _py/python.py
@@ -7,10 +7,9 @@ with open("favorites.csv", "r") as file:
 
     for row in reader:
         title = row["title"].strip().upper()
-        if title in titles:
-            titles[title] += 1
-        else:
-            titles[title] = 1
+        if not title in titles:
+            titles[title] = 0
+        titles[title] += 1
 
 for title in sorted(titles):
     print(title)
