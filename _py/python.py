291b1c528b1ca44c2c2cab9c39e76f187c078cc5
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-11 00:50:00 +0000

    ddd, Nov 11/11/2022, YYYY, 12:50 A +0

--- _py/python.py
+++ _py/python.py
@@ -6,6 +6,7 @@ with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
+        title = row["title"].strip().upper()
         if not row["title"] in titles:
             titles.append(row["title"])
 
