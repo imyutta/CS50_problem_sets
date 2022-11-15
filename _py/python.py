7064d8cc45f009ec6d76560c6605e1d778605680
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-15 01:01:12 +0000

    ddd, Nov 11/15/2022, YYYY, 1:01 A +0

--- _py/python.py
+++ _py/python.py
@@ -8,6 +8,7 @@ with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
+       
         if row["title"].strip().upper() == title:
             counter += 1
 
