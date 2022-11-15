8729f899b1a51d15aaeb902bf751f07fe5b5e61a
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-15 00:58:29 +0000

    ddd, Nov 11/15/2022, YYYY, 12:58 A +0

--- _py/python.py
+++ _py/python.py
@@ -8,9 +8,8 @@ with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
-        title = row["title"].strip().upper()
-        if re.search("^(OFFICE|THE.OFFICE)$", title):
+        if row["title"].strip().upper() == title:
             counter += 1
-        titles = title
 
-print(f"Number of people who like The Office: {counter}")
+
+print(counter)
