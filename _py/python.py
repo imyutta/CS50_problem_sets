ad3f509f5274cf04d67254fedb323fc5029169cc
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-15 00:47:42 +0000

    ddd, Nov 11/15/2022, YYYY, 12:47 A +0

--- _py/python.py
+++ _py/python.py
@@ -1,32 +1,15 @@
-# import csv
-# import re
-# titles = {}
-# counter = 0
-
-# with open("favorites.csv", "r") as file:
-#     reader = csv.DictReader(file)
-
-#     for row in reader:
-#         title = row["title"].strip().upper()
-#         #if re.search("^(OFFICE|THE OFFICE)$", title):
-#            # counter += 1
-#         titles = title
-
-# #print(f"Number of people who like The Office: {counter}")
-# print(title)
-
 import csv
+import re
 
-titles = {}
+counter = 0
 
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
         title = row["title"].strip().upper()
-        if not title in titles:
-            titles[title] = 0
-        titles[title] += 1
+        if re.search("^(OFFICE|THE OFFICE)$", title):
+            counter += 1
+        titles = title
 
-for title in sorted(titles):
-    print(title, titles[title])
+print(f"Number of people who like The Office: {counter}")
