4c19abaf32b67c89850dbddab7aec5af8f0f4fb5
Yutta <105465153+imyutta@users.noreply.github.com>
2022-11-15 00:45:10 +0000

    ddd, Nov 11/15/2022, YYYY, 12:45 A +0

--- _py/python.py
+++ _py/python.py
@@ -1,7 +1,6 @@
 import csv
 import re
-
-
+titles = {}
 counter = 0
 
 with open("favorites.csv", "r") as file:
@@ -9,7 +8,25 @@ with open("favorites.csv", "r") as file:
 
     for row in reader:
         title = row["title"].strip().upper()
-        if re.search("^(OFFICE|THE OFFICE)$", title):
-            counter += 1
+        #if re.search("^(OFFICE|THE OFFICE)$", title):
+           # counter += 1
+        titles = title
 
-print(f"Number of people who like The Office: {counter}")
+#print(f"Number of people who like The Office: {counter}")
+print(title)
+
+import csv
+
+titles = {}
+
+with open("favorites.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        title = row["title"].strip().upper()
+        if not title in titles:
+            titles[title] = 0
+        titles[title] += 1
+
+for title in sorted(titles):
+    print(title, titles[title])
