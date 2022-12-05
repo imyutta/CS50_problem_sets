217147c7a9bf1c00f3d9425e819ec063767d24a8
Yutta <105465153+imyutta@users.noreply.github.com>
2022-12-05 21:37:56 +0000

    Mon, Dec 5, 2022, 9:37 PM +00:00

--- _py/python.py
+++ _py/python.py
@@ -2,5 +2,11 @@ import csv
 
 from cs50 import SQL
 
-db = sql("sqlite3:///fa1.db")
-title = input("Title: ").s
+db = SQL("sqlite3:///fa1.db")
+title = input("Title: ").strip()
+
+rows = db.execute("SELECT COUNT(*) AS counter FROM favorites WHERE title LIKE ?", title)
+
+row = rows[0]
+
+print(row["counter"])
