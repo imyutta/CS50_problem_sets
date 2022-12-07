a6c61da5285aca0a1e5854e95fad8fee4788dc9a
Yutta <105465153+imyutta@users.noreply.github.com>
2022-12-07 06:35:25 +0000

    Wed, Dec 7, 2022, 6:35 AM +00:00

--- _py/python.py
+++ _py/python.py
@@ -9,4 +9,5 @@ rows = db.execute("SELECT title FROM favo WHERE title LIKE ?", title)
 
 for row in rows:
     print(row["title"])
+    SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON movies.id = stars.movie_id  WHERE title = "Toy Story";
 
