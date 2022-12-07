53a49223e803cf9d815ac87b417e1897b9be1c60
Yutta <105465153+imyutta@users.noreply.github.com>
2022-12-07 21:35:45 +0000

    Wed, Dec 7, 2022, 9:35 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -2,4 +2,8 @@
     FROM people
    WHERE id IN
  (SELECT person_id
- FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE title = "Toy Story"));
+    FROM stars
+   WHERE movie_id IN
+ (SELECT id
+    FROM movies
+   WHERE title = "Toy Story"));
