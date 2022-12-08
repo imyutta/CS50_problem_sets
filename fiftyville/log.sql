94538e44caf4b9490e8572ae2513cd34c8a0cb08
Yutta <105465153+imyutta@users.noreply.github.com>
2022-12-08 04:08:54 +0000

    Thu, Dec 8, 2022, 4:08 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+
+-- FOUND crime time: 10:15
+-- FOUND flight date: 29 July 2021
+-- FOUND flight origin: fiftyville
+
+
 SELECT * -- find the reports (parameters: date, street)
   FROM crime_scene_reports
  WHERE street LIKE '%Humphrey%'
@@ -14,7 +20,8 @@ SELECT * -- find the report (parameters: date, street, keyword "duck:)
        AND month = 7
        AND day = 28
        AND description LIKE "%duck%";
--- time of the crime 10:15 am
+
+-- FOUND crime time 10:15
 
 SELECT transcript -- Find interview transcripts
   FROM interviews
@@ -22,13 +29,14 @@ SELECT transcript -- Find interview transcripts
        AND year = 2021
        AND month = 7
        AND day = 28;
+-- FOUND flight date: 29 July 2021
+-- FOUND flight origin: fiftyville
 
 SELECT * -- take a look at bakery security logs (year, month,day)
   FROM bakery_security_logs
  WHERE year = 2021
        AND month = 7
        AND day = 28;
-
 
 SELECT * -- take a CLOTHER look at bakery security logs (year, month,day, hour, minute, activity)
   FROM bakery_security_logs
@@ -39,15 +47,12 @@ SELECT * -- take a CLOTHER look at bakery security logs (year, month,day, hour, 
        AND minute >= 15
        AND minute <= 25;
 
--- found some license_plates
-
 SELECT * -- Take a look at ATM transavtions (year, month, day, atm_location)
   FROM atm_transactions
  WHERE year = 2021
        AND month = 7
        AND day = 28
        AND atm_location = "Leggett Street";
-
 
 SELECT * -- Take a look at ATM transavtions (year, month, day, atm_location, transaction_type)
   FROM atm_transactions
@@ -69,3 +74,4 @@ SELECT * -- check for flights (year, month, day)
  WHERE year = 2021
       AND month = 7
       AND day = 29
+      AND 
