21f3f759ea34bc79daf586557769050b6e396eee
Yutta <105465153+imyutta@users.noreply.github.com>
2022-12-08 01:57:15 +0000

    Thu, Dec 8, 2022, 1:57 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT id
+SELECT *
   FROM crime_scene_reports
- WHERE street = 'Humphrey';
+ WHERE street = '%Humphrey%' AND year = 2021 AND month = "July";
