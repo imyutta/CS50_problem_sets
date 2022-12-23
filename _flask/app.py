0e67f55244277a48bd4c1954b3ff27ab1e457ccc
Yutta <105465153+imyutta@users.noreply.github.com>
2022-12-23 00:52:14 +0000

    Fri, Dec 23, 2022, 12:52 AM +00:00

--- _flask/app.py
+++ _flask/app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    name = request.args.get("name")
-    return render_template("index.html", name=name)
+ @app.route("/")
+  def index():
+      return render_template("index.html")
 
+
+  @app.route("/greet")
+  def greet():
+      name = request.args.get("name")
+      return render_template("greet.html", name=name)
