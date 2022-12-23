c47959532269b9ec9580e0757be71c2d1e9ec911
Yutta <105465153+imyutta@users.noreply.github.com>
2022-12-23 06:20:41 +0000

    Fri, Dec 23, 2022, 6:20 AM +00:00

--- _flask/app.py
+++ _flask/app.py
@@ -8,7 +8,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/greet")
+@app.route("/greet", methods=["post"])
 def greet():
     name = request.form.get("name", "world")
     return render_template("greet.html", name=name)
