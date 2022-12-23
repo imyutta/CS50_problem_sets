3be2cb306e10b9e00ae1fae99936dfc49980021a
Yutta <105465153+imyutta@users.noreply.github.com>
2022-12-23 23:09:19 +0000

    Fri, Dec 23, 2022, 11:09 PM +00:00

--- _flask/app.py
+++ _flask/app.py
@@ -6,9 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/greet", methods=["post"])
-def greet():
-    name = request.form.get("name", "world")
-    return render_template("greet.html", name=name)
