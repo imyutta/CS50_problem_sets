b297aec71f32ea0827a45ff9cacc667997d5eb2a
Yutta <105465153+imyutta@users.noreply.github.com>
2023-01-14 02:00:49 +0000

    Sat, Jan 14, 2023, 2:00 AM +00:00

--- _flask2/app.py
+++ _flask2/app.py
@@ -5,11 +5,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
