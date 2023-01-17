b6e66554bc1d82954867c6f6173905a67b2ca922
Yutta <105465153+imyutta@users.noreply.github.com>
2023-01-17 02:40:37 +0000

    Tue, Jan 17, 2023, 2:40 AM +00:00

--- _flask2/froshims/app.py
+++ _flask2/froshims/app.py
@@ -1,8 +1,10 @@
-# Implements a registration form using a select menu
+# Implements a registration form, storing registrants in a dictionary, with error messages
 
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
+
+REGISTRANTS = {}
 
 SPORTS = [
     "Basketball",
@@ -19,9 +21,25 @@ def index():
 @app.route("/register", methods=["POST"])
 def register():
 
-    # Validate submission
-    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
-        return render_template("failure.html")
+    # Validate name
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+
+    # Validate sport
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+
+    # Remember registrant
+    REGISTRANTS[name] = sport
 
     # Confirm registration
-    return render_template("success.html")
+    return redirect("/registrants")
+
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
