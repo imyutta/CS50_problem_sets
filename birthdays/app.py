8c7b761deed9926ce438a9d73f2dd1cc0a0cfeb7
Yutta <105465153+imyutta@users.noreply.github.com>
2023-02-03 01:31:26 +0000

    Thu, Feb 2, 2023, 5:31 PM -08:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,7 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        friends = db.execute("SELECT * FROM index")
+        friends = db.execute("SELECT * FROM birthdays")
 
         return render_template("index.html", friends=friends)
 
