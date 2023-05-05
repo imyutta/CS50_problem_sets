9fdf51a36569754c96918a49f9c92323e2425edf
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-05 22:39:54 +0000

    Fri, May 5, 2023, 3:39 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -132,7 +132,7 @@ def register():
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-        # Ensure username exists and password is correct
+        # Ensure username does not existin the database
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
