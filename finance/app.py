26f590ebe9f3c793421b1af36a35894db930da6b
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-05 22:39:07 +0000

    Fri, May 5, 2023, 3:39 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -117,7 +117,7 @@ def register():
 
     # Forget any user_id
     session.clear()
-    
+
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
@@ -128,6 +128,13 @@ def register():
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
