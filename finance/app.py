b8dc2cb157ab54851075ffd5be39b08b5541c0be
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-05 03:48:35 +0000

    Thu, May 4, 2023, 8:48 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -117,7 +117,15 @@ def register():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
         
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("registration.html")
