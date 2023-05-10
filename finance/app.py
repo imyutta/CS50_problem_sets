8e96fedfe0daef9186be302a2a725acdc615dd17
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-10 22:28:22 +0000

    Wed, May 10, 2023, 3:28 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -110,7 +110,7 @@ def quote():
     """Get stock quote."""
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
+        lookup()
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("quote.html")
