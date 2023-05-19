d79e91f1b904ec897510095374a3062fb1ff6835
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-19 01:17:47 +0000

    Thu, May 18, 2023, 6:17 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -61,6 +61,8 @@ def buy():
         if number_of_shares is None or number_of_shares == '' or number_of_shares < 1:
             return apology("must provide a valid number of shares", 403)
 
+        # Look up a stock's current price
+        
         # Redirect user to home page
         return redirect("/")
 
