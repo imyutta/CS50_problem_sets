b299ce5042119286c5a43efde1b8596742e9868f
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-19 00:57:59 +0000

    Thu, May 18, 2023, 5:57 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -52,7 +52,8 @@ def buy():
     if request.method == "POST":
         # Collect the data:
         quotes = lookup(request.form.get("symbol"))
-
+        shares = request.form.get("shares")
+        
         # Make sure the symbol exists
         if not quotes:
             return apology("Invalid symbol", 403)
@@ -123,12 +124,10 @@ def quote():
     if request.method == "POST":
         # Collect the data:
         quotes = lookup(request.form.get("symbol"))
-        shares = request.form.get("shares")
 
         # Make sure the symbol exists
         if not quotes:
             return apology("Invalid symbol", 403)
-        elif shares 
 
         # If the symbol exists, show user the quote price
         return render_template("quoted.html", quotes=quotes)
