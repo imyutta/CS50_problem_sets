c9abf21785d31f1357570701fcbd825696be8fda
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-24 22:38:01 +0000

    Wed, May 24, 2023, 3:38 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -82,6 +82,7 @@ def buy():
         else:
             # do something in new database
             if no new database
+            db.execute("INSERT INTO purchases SET ")
             # renew users cash data
             cash_renewed = cash[0]["cash"] - total_price
             db.execute("UPDATE users SET cash TO ? WHERE id = ?", cash_renewed, user_id)
