4ba2ca2243662072cde960df247dfaa6e4166f6c
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-25 21:17:10 +0000

    Thu, May 25, 2023, 2:17 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -80,14 +80,15 @@ def buy():
         if cash[0]["cash"] < total_price:
             return apology("not enough cash", 403)
         else:
+            # Calculate how much cash will user have after purchase
+            cash_renewed = cash[0]["cash"] - total_price
+
             # find users data in the purchase database
             users_purchases = db.execute("SELECT * FROM purchases WHERE id = ?", user_id)
             # If it is a first buy order from this user, insert him to purchases database
             if not users_purchases:
                 db.execute("INSERT INTO purchases (id, symbol, price, amount) VALUES ?, ?, ?, ?", user_id, symbol, share_price, number_of_shares)
 
-                # renew users cash data
-                cash_renewed = cash[0]["cash"] - total_price
                 db.execute("UPDATE users SET cash TO ? WHERE id = ?", cash_renewed, user_id)
             # If user is already exist in the purchase database, just updte the purchase database
             else:
@@ -97,22 +98,15 @@ def buy():
                     db.execute("UPDATE purchases SET (price, amount) TO ?, ? WHERE id = ? AND symbol = ?", share_price, number_of_shares, user_id, symbol)
 
                     # renew users cash data
-                    cash_renewed = cash[0]["cash"] - total_price
                     db.execute("UPDATE users SET cash TO ? WHERE id = ?", cash_renewed, user_id)
                     return redirect("/")
                 else:
                     # If user is buying this share for the first time, add the data to the purchases database
                     db.execute("INSERT INTO purchases (id, symbol, price, amount) VALUES ?, ?, ?, ?", user_id, symbol, share_price, number_of_shares)
-                    db.execute("UPDATE users SET cash TO ? WHERE id = ?" )
+                    db.execute("UPDATE users SET cash TO ? WHERE id = ?", cash_renewed, user_id)
 
-
-
-
-
-
-
-        # Redirect user to home page
-        return redirect("/")
+                    # Redirect user to home page
+                    return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
