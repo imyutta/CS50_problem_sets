bef90203c13a0dfdc9b0807420282889893984f0
Yutta <105465153+imyutta@users.noreply.github.com>
2023-05-25 22:01:45 +0000

    Thu, May 25, 2023, 3:01 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -60,13 +60,10 @@ def buy():
         number_of_shares = request.form.get("shares")
         # Check if the number of shares provided by the user is digit
         if not number_of_shares.isdigit():
-            return apology("a number of shares should be a digit", 403)
+            return apology("a number of shares should be a positive number", 403)
 
         # Convert the number of shares from string to an integer
         number_of_shares = float(number_of_shares)
-        if number_of_shares < 1:
-            return apology("a number of shares should be a positive number", 403)
-
 
         # Look up a stock's current price
         quotes = lookup(symbol)
