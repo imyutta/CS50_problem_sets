95e8008672f7cfb5c9e782523c0fd9e08d4b0f18
Yutta <105465153+imyutta@users.noreply.github.com>
2023-07-24 23:14:09 +0000

    Mon, Jul 24, 2023, 4:14 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -42,7 +42,9 @@ def index():
     # Get user's stocks, user's numbers of shares:
     users_stocks = db.execute("SELECT symbol, SUM(amount) as total_amount FROM purchases WHERE id = ? GROUP BY symbol HAVING amount > 0", users_id)
 
-    # Get user's cash
+    # Get user's cash:
+    users_cash = db.execute("SELECT cash FROM users WHERE id = ?", users_id)
+    
     # the current price of each stock, and the total value of each holding
     return apology("TODO")
 
