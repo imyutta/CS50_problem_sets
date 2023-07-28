b2856f81b8e69343a809098156f9f29816e85137
Yutta <105465153+imyutta@users.noreply.github.com>
2023-07-28 02:50:34 +0000

    Thu, Jul 27, 2023, 7:50 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -262,7 +262,8 @@ def sell():
 
         # Check if (somehow, once submitted) the user does not own any shares of that stock:
         # Query the database for users purchases with this symbol:
-        
+        users_share = db.execute("SELECT * FROM purchases WHERE users_id = ?", users_id)
+
 
         # Take the number of shares user wants to sell:
         number_of_shares = request.form.get("shares")
