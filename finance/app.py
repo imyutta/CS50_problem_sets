e4c367762c87cb6c56b47e3060e8838e410615f5
Yutta <105465153+imyutta@users.noreply.github.com>
2023-08-12 01:03:01 +0000

    Fri, Aug 11, 2023, 6:03 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
@@ -7,6 +8,8 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
+
+from datetime import datetime
 
 # Configure application
 app = Flask(__name__)
