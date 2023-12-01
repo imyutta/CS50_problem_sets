a2f4e91f6bdc3e6e4799f5ff96b9259ba4037a6f
Yutta <105465153+imyutta@users.noreply.github.com>
2023-12-01 07:12:19 +0000

    Thu, Nov 30, 2023, 11:12 PM -08:00

--- todoapp/app.py
+++ todoapp/app.py
@@ -3,7 +3,7 @@ import sqlite3
 import re
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session jsonify
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
